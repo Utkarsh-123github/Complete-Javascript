1c298a74623cc7a1fa50fcb04d85872303f07d19
Utkarsh Tiwari <utk0703@gmail.com>
2024-02-26 12:38:33 +0530

    variable and some operations

---
+++ basics/02_variable-conversion.js
@@ -0,0 +1,8 @@
+let score = 10
+console.log(typeof score);
+
+let scoreInString = String(score)
+console.log(typeof scoreInString)
+
+console.log(typeof null) // null is an object data type
+console.log(typeof undefined) // undefined has undefined data type
---
+++ basics/03_some-crazy-meme-operations.js
@@ -0,0 +1,17 @@
+console.log("1" + 2 ) //12
+console.log(1 + 1 + "2" ) //22
+console.log( 1 + 2 ) //3
+console.log("1" + 2 + 2 ) //122
+
+
+
+// In sab ka logic simple hai
+// Jo pehli data type hoti hai , operation usi ke according hota hai
+// first wale me pehla data type string hai isliye baki numbers string bana ke concatenate krdiya hai
+// second wale me pehla data type number hai isliye pehle addition hua hai fir string aaya hai isliye concatenation hua hai
+// fourth wale me shuru me hi string mil gya hai isliye baki sab ko bhi string bana diya hai or concatenate kr diya hai
+
+
+// Note :- JS offers two categories of data types -->
+// Primitive Data types:- numbers, strings, boolean, symbol, null, undefined, BigInt
+// Non primitive or reference data type:- array, objects, functions
