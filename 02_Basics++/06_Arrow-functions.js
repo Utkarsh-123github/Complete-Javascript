d4e7b635e4b31023728c7eb428c5c7e83e2d0d3a
Utkarsh Tiwari <utk0703@gmail.com>
2024-02-29 21:46:55 +0530

    Arrow-function-intro

---
+++ 02_Basics++/06_Arrow-functions.js
@@ -0,0 +1,13 @@
+function add(num1,num2){
+    return num1 + num2
+}
+
+console.log(add(3,4))
+
+// Implementing using arrow function
+
+let addition = (num1,num2)=>{
+    return num1 + num2
+}
+
+console.log(addition(2,3))
