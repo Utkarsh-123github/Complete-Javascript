dd614e550e0fcd311a1f28170a891abf53b428b1
Utkarsh Tiwari <utk0703@gmail.com>
2024-03-02 09:14:13 +0530

    filter

---
+++ 02_Basics++/09_filter.js
@@ -0,0 +1,19 @@
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  // print all the books that are published after 1989
+
+  const arr = books.filter( (item)=>{
+    return item.publish >= 1989
+  } )
+
+  console.log(arr)
