4f89ce93c31f1adf3313bf7e0ab487aae4883806
Utkarsh Tiwari <utk0703@gmail.com>
2024-03-06 13:39:16 +0530

    BMI generator

--- 03_Basic_DOM_Project/02_BMI_generator/index.js
+++ 03_Basic_DOM_Project/02_BMI_generator/index.js
@@ -1,1 +1,16 @@
-// hello
+const forms = document.querySelector('form')
+forms.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = "Please enter a valid height !!"
+    } else if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = "Please enter a valid weight !!"
+    } else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+
+});
